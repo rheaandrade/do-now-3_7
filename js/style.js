b507a0db3fbc49d0724f27bc97be8a8421260ee2
rheaandrade <andrade.rhea@gmail.com>
2016-03-07 09:40:00 -0500

    Added files via upload

---
+++ js/style.js
@@ -0,0 +1,10 @@
+/* java scripts for wave effect parallax and scrollspy demo */
+
+$(document).ready(function(){
+
+    /* For parallaxing images */
+    $('.parallax').parallax();
+
+    /* To scroll to a section from navbar */
+    $('.scrollspy').scrollSpy();
+});
